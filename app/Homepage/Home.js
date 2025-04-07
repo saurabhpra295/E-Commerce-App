f9a1c92873a3f900d05409eb888af18aeada6950
saurabhpra295 <saurabhprakash295@gmail.com>
2025-04-07 19:02:19 +0530

    changes in homepage

--- app/Homepage/Home.js
+++ app/Homepage/Home.js
@@ -4,39 +4,8 @@ const Home = () => {
   return (
     <div className='flex text-center bg-red-400 p-10'>
       <Link href="/Homepage">
-      If you have ever used React before you have worked with tuples more than likely.
-
-useState returns a tuple of the value and a setter function.
-
-const [firstName, setFirstName] = useState('Dylan') is a common example.
-
-Because of the structure we know our first value in our list will be a certain value type in this case a string and the second value a function.
-      
-If you have ever used React before you have worked with tuples more than likely.
-
-useState returns a tuple of the value and a setter function.
-
-const [firstName, setFirstName] = useState('Dylan') is a common example.
-
-Because of the structure we know our first value in our list will be a certain value type in this case a string and the second value a function.
-
-If you have ever used React before you have worked with tuples more than likely.
-
-useState returns a tuple of the value and a setter function.
-
-const [firstName, setFirstName] = useState('Dylan') is a common example.
-
-Because of the structure we know our first value in our list will be a certain value type in this case a string and the second value a function.
-
-If you have ever used React before you have worked with tuples more than likely.
-
-useState returns a tuple of the value and a setter function.
-
-const [firstName, setFirstName] = useState('Dylan') is a common example.
-
-Because of the structure we know our first value in our list will be a certain value type in this case a string and the second value a function.
-
-    </Link>
+        Homepage
+     </Link>
   )
 
 
--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => (
   <footer className="bg-gray-800 p-4 text-white text-center">
-    &copy; 2025 Saurabh E-Commerce App
+     2025 Saurabh E-Commerce App
   </footer>
 );
 
